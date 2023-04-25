4bc9b9764796caea809630b0e68580a90a1849d7
Rose Black <o2bablackrose@gmail.com>
2023-04-25 18:47:06 -0400

    latest changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
-const PORT = 3009 || process.env.PORT;
+const PORT = process.env.PORT || 3009;
 
 const app = express();
 
